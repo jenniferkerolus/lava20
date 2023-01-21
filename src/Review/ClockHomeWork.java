6244c2a65520b4ff4d353054481d7604db8dd524
jenniferkerolus <jennifer.kerolus@gmail.com>
2023-01-21 00:20:58 -0500

    v6

---
+++ src/Review/ClockHomeWork.java
@@ -0,0 +1,16 @@
+package Review;
+
+public class ClockHomeWork {
+    public static void main(String[] args) {
+        for (int i = 0; i <=23; i++) {
+            for (int j = 0; j <=5; j++) {
+                for (int k = 0; k <=9; k++) {
+                    System.out.println(i+":"+j+k);
+
+                }
+
+            }
+
+        }
+    }
+}
