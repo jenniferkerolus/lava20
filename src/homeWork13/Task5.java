f2e1ba0388251a61d09bc7943c2eeb22e93fe5dd
jenniferkerolus <jennifer.kerolus@gmail.com>
2023-01-25 02:49:31 -0500

    HomeWork13

--- src/homeWork13/Task5.java
+++ src/homeWork13/Task5.java
@@ -2,7 +2,14 @@ package homeWork13;
 
 public class Task5 {
     public static void main(String[] args) {
+        String str="JAVA";
+        String reversed=new StringBuilder(str).reverse().toString();
+        if (str.equals(reversed)) {
+            System.out.println("This String is a palindrome");
+        }else{
+            System.out.println("Is not a palindrome");
+        }
         }
 
         }
-    
+
